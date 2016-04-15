00b88e528af3830961d70a41ad213a557096e30e
Naveen <p6naveen@gmail.com>
2016-04-15 18:28:19 +0530

    tests folder added

---
+++ tests/CountryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Country
+ */
+
+namespace CountryCodeTest;
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+class CountryTest extends \PHPUnit_Framework_TestCase {
+
+    function __construct() {
+        $this->countryObj = new \CountryCode\Country();
+    }
+
+    function testSetup() {
+        $this->countryObj->Setup('IN');
+        $this->assertEquals('IN', $this->countryObj->country, "Country Code Setup Test");
+    }
+
+}
