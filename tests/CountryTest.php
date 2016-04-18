6a50440c5ad076f9a5f827a13c16f238beb96e42
Naveen <p6naveen@gmail.com>
2016-04-18 17:08:39 +0530

    test class updated

--- tests/CountryTest.php
+++ tests/CountryTest.php
@@ -19,4 +19,8 @@ class CountryTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals('IN', $this->countryObj->country, "Country Code Setup Test");
     }
 
+    function testCheckCountryInCsv() {
+        $this->assertEquals(true, $this->countryObj->CheckCountryInCsv("IN"), "Country exists in csv test");
+    }
+
 }
